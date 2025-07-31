076cab0a9aed96b95db4eeb6148bac3110eed994
Annu Singh <annu37752@gmail.com>
2025-07-31 11:37:19 +0530

    tranding

---
+++ src/components/Card.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Card({ title, data }) {
+  return (
+    <div className="text-white w-full justify-center min-h-screen mt-1 flex items-start gap-5 flex-wrap ">
+      {data.map((s, i) => (
+        <Link to="/tranding" key={i} className="w-[40vh] mt-8 rounded-lg shadow ">
+          <div className="w-full bg-amber-100 rounded-t-lg h-[58vh] overflow-hidden ">
+            <img
+              className="w-full h-full object-cover  "
+              src={`https://image.tmdb.org/t/p/original/${
+                s.backdrop_path || s.profile_path || s.poster_path
+              }`}
+              alt="images show only"
+            />
+          </div>
+          <h1 className="mt-1 tracking-tight leading-none text-md font-semibold ">
+            { s.title || s.name || s.original_name || s.original_title }
+          </h1>
+        </Link>
+      ))}
+    </div>
+  );
+}
+
+export default Card;
--- src/components/Tranding.jsx
+++ src/components/Tranding.jsx
@@ -1,11 +1,80 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { GoArrowLeft } from "react-icons/go";
+import TopNavigation from "./tamplete/TopNavigation";
+import Selector from "./tamplete/Selector";
+import { useNavigate } from "react-router-dom";
+import axios from "../utils/axios";
+import Card from "./Card";
+import Loading from "./Loading";
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 const Tranding = () => {
-  return (
-    <div>
-      trending
-    </div>
-  )
-}
+  const navigate = useNavigate();
 
-export default Tranding
+  const [category, setCategory] = useState("all");
+  const [duration, setDuration] = useState("day");
+  const [tranding, setTranding] = useState([]);
+
+  const gettranding = async () => {
+    try {
+      const { data } = await axios.get(`/trending/${category}/${duration}`);
+      //setTranding(data.results);
+      setTranding(prevState => [...prevState, ...data.results])
+      console.log(data)
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  console.log(tranding)
+
+  useEffect(() => {
+    console.log(tranding);
+    gettranding();
+  }, [category, duration]);
+
+  return tranding.length > 0 ? (
+    <div className="p-4 min-h-screen w-full ">
+      <div className="flex items-center">
+        <div className="flex items-center gap-2">
+          <span
+            onClick={() => navigate(-1)}
+            className="text-white font-semibold mt-1 text-2xl hover:text-blue-500 cursor-pointer"
+          >
+            <GoArrowLeft />
+          </span>
+          <h1 className="text-white capitalize text-xl tracking-tight font-semibold ">
+            trending
+          </h1>
+        </div>
+        <div className="w-full flex items-center">
+          <div className="w-[70%] text-zinc-300">
+            <TopNavigation />
+          </div>
+          <div className="flex items-center gap-4">
+            <Selector
+              title="tranding"
+              option={["tv", "movie", "all"]}
+              funct={(e) => setCategory(e.target.value)}
+            />
+            <Selector
+              title="duration"
+              option={["week", "day"]}
+              funct={(e) => setDuration(e.target.value)}
+            />
+          </div>
+        </div>
+      </div>
+      <InfiniteScroll
+      dataLength={tranding.length}
+      loader={<h4>Loading...</h4>}
+      hasMore={true}
+      next={gettranding}
+      >
+      <Card title={category} data={tranding} />
+      </InfiniteScroll>
+    </div>
+  ) : <Loading />
+};
+
+export default Tranding;
